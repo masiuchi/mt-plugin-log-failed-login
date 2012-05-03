918b74a9aadf222961e2281bcbd3933021730f67
masiuchi <masahiro.iuchi@gmail.com>
2012-05-03 10:29:58 +0900

    tidied

--- plugins/LogFailedLogin/LogFailedLogin.pl
+++ plugins/LogFailedLogin/LogFailedLogin.pl
@@ -12,11 +12,11 @@ our $VER  = '0.01';
 our $NAME = ( split /::/, __PACKAGE__ )[-1];
 
 my $plugin = __PACKAGE__->new({
-    name => $NAME,
-    id => lc $NAME,
-    key => lc $NAME,
-    l10n_class => $NAME . '::L10N',
-    version => $VER,
+    name        => $NAME,
+    id          => lc $NAME,
+    key         => lc $NAME,
+    l10n_class  => $NAME . '::L10N',
+    version     => $VER,
     author_name => 'masiuchi',
     author_link => 'https://github.com/masiuchi/',
     plugin_link => 'https://github.com/masiuchi/mt-plugin-log-failed-login/',
